8150de164674ef364d10c9cbea70c63c31fc22e9
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-01-17 10:51:42 +0200

    Basic mvc model

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+class Model {
+	constructor(){}
+}
+class View {
+	constructor(){}
+}
+class controller {
+	constructor(model, view){
+	this.model = model
+	this.view = view
+}
+}
+const app = new controller(new Model(), new View())
